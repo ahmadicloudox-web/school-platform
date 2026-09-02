40694338dbeb63579aff50cc2de0fac18264e47b
Ahmad Hussein <ahmad.icloudox@gmail.com>
2026-09-03 00:25:25 +0300

    Fix routing for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 // src/index.js أو src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
+    <HashRouter
       future={{
         v7_startTransition: true,      // ✅ لإزالة التحذير الأول
         v7_relativeSplatPath: true,    // ✅ لإزالة التحذير الثاني
       }}
     >
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
